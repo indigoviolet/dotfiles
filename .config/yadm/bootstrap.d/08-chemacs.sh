560706737bdcff5157b7ade9abe33b776f09819d
Venky Iyer <indigoviolet@gmail.com>
2021-06-07 11:13:03 -0700

    remove alt files

--- .config/yadm/bootstrap.d/08-chemacs.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-# chemacs
-
-
-# [[file:../../../README.org::*chemacs][chemacs:1]]
-set -eux
-git clone https://github.com/plexus/chemacs.git "${HOME}/.local/chemacs" && $HOME/.local/chemacs/install.sh
-# chemacs:1 ends here
