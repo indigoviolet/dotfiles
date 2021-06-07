9bc644b8b6f29a7bf8f7a0d63487e071165a1889
Venky Iyer <indigoviolet@gmail.com>
2021-06-07 16:32:40 -0700

    Mac boostrap

--- .config/yadm/bootstrap.d/06-zinit.sh
+++ .config/yadm/bootstrap.d/06-zinit.sh
@@ -4,5 +4,5 @@
 
 # [[file:../../../README.org::*Zinit][Zinit:1]]
 set -eux
-mkdir ~/.zinit && git clone https://github.com/zdharma/zinit.git ~/.zinit/bin
+mkdir ~/.zinit && git clone https://github.com/zdharma/zinit.git ~/.zinit/bin || true
 # Zinit:1 ends here
