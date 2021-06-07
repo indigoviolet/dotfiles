3a35eae347fa6c69535623530d45e3575ad73ceb
Venky Iyer <indigoviolet@gmail.com>
2021-06-06 20:28:32 -0700

    move some stuff from old README

--- .config/yadm/bootstrap.d/02-brew.sh
+++ .config/yadm/bootstrap.d/02-brew.sh
@@ -1,10 +1,15 @@
 #/bin/bash
 # Brew
 
-# =brew bundle dump --global -f=
+# =brew bundle dump --global --force --describe=
 
 
 # [[file:../../../README.org::*Brew][Brew:1]]
 set -eux
+
+# install brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+# install items
 brew bundle --global check || brew bundle --global install -v
 # Brew:1 ends here
--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1328,6 +1328,30 @@ https://code.orgmode.org/bzg/org-mode/commit/13424336a6f30c50952d291e7a82906c121
   )
 ;; magit:1 ends here
 
+;; Handle bare repos (yadm/$HOME)
+
+;; Yadm (yadm.io) keeps its git directory in .local/share/yadm/repo.git (found this
+;; by running =yadm=). This teaches Magit to use the right git directory for $HOME
+
+;; https://github.com/magit/magit/issues/460#issuecomment-837449105
+
+
+;; [[file:config.org::*Handle bare repos (yadm/$HOME)][Handle bare repos (yadm/$HOME):1]]
+(defun home-magit-process-environment (env)
+  "Add GIT_DIR and GIT_WORK_TREE to ENV when in a special directory.
+https://github.com/magit/magit/issues/460 (@cpitclaudel)."
+  (let ((default (file-name-as-directory (expand-file-name default-directory)))
+        (home (expand-file-name "~/")))
+    (when (string= default home)
+      (let ((gitdir (expand-file-name "~/.local/share/yadm/repo.git/")))
+        (push (format "GIT_WORK_TREE=%s" home) env)
+        (push (format "GIT_DIR=%s" gitdir) env))))
+  env)
+
+(advice-add 'magit-process-environment
+            :filter-return #'home-magit-process-environment)
+;; Handle bare repos (yadm/$HOME):1 ends here
+
 ;; [[file:config.org::*LSP][LSP:2]]
 (use-package! lsp-mode
   :init
