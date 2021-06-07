8690e9b74b0710256c345ae62b418b9ab2ffc22e
Venky Iyer <indigoviolet@gmail.com>
2021-06-07 14:28:56 -0700

    brew is needed to bootstrap

--- .config/yadm/bootstrap.d/02-brew.sh
+++ .config/yadm/bootstrap.d/02-brew.sh
@@ -9,9 +9,6 @@
 # [[file:../../../README.org::*Brew][Brew:1]]
 set -eux
 
-# install brew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-
 # install items
 if [[ -e ~/.Brewfile ]]; then
 	brew bundle --global check || brew bundle --global install -v
