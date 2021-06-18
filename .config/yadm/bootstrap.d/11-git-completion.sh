ee5a60888945e2a4de9446186d0a9b1f7e9dade5
Venky Iyer <indigoviolet@gmail.com>
2021-06-17 17:23:02 -0700

    include bootstrap file

---
+++ .config/yadm/bootstrap.d/11-git-completion.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# git completion
+
+
+# [[file:../../../README.org::*git completion][git completion:1]]
+set -ux
+curl -o ${ZSH_CUSTOM_DIR}/git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+curl -o ${ZSH_CUSTOM_DIR}/_git https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh
+# git completion:1 ends here
