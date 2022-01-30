7fe66ed7ca3017c7d821a44c09cb9a8770a4c30e
Venky Iyer <indigoviolet@gmail.com>
2022-01-29 19:22:11 -0800

    add instructions and include start.sh

---
+++ .config/yadm/start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# [[file:../../README.org::*Get started][Get started:2]]
+set -eux
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install yadm
+yadm clone git@github.com:indigoviolet/dotfiles-yadm.git
+# Get started:2 ends here
