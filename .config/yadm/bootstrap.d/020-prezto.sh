2f40c7913ab434548759e86dcd07aa5d41f815ee
Venky Iyer <indigoviolet@gmail.com>
2022-02-02 15:39:27 -0800

    move poetry completion to misc utilities

--- .config/yadm/bootstrap.d/020-prezto.sh
+++ .config/yadm/bootstrap.d/020-prezto.sh
@@ -7,7 +7,4 @@ set -ux
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${HOME}/.zprezto"
 git clone --recurse-submodules https://github.com/belak/prezto-contrib "${HOME}/.zprezto/contrib"
 cd ~/.zprezto/contrib && git pull
-
-## poetry completion (https://python-poetry.org/docs/master/#enable-tab-completion-for-bash-fish-or-zsh)
-poetry completions zsh > ~/.zprezto/modules/completion/external/src/_poetry
 # Prezto:1 ends here
--- .config/yadm/bootstrap.d/070-misc-utilities.sh##distro.Ubuntu,e.sh
+++ .config/yadm/bootstrap.d/070-misc-utilities.sh##distro.Ubuntu,e.sh
@@ -10,4 +10,10 @@ set -eux
 # this is installed with apt-get because brew installs a shitload of dependencies
 (command -v svn &>/dev/null) || sudo apt-get install --yes subversion
 curl https://raw.githubusercontent.com/gitbits/git-info/master/git-info --output ~/.local/bin/git-info && chmod +x ~/.local/bin/git-info
+
+
+
+## poetry completion in prezto (https://python-poetry.org/docs/master/#enable-tab-completion-for-bash-fish-or-zsh)
+## poetry installed with asdf
+poetry completions zsh > ~/.zprezto/modules/completion/external/src/_poetry
 # Misc utilities:1 ends here
