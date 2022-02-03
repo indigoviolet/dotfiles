5858a00b5eeaee30247269169fb48319190d5679
Venky Iyer <indigoviolet@gmail.com>
2022-02-02 16:00:43 -0800

    don't install recommends with apt -y

--- .config/yadm/bootstrap.d/070-misc-utilities.sh##distro.Ubuntu,e.sh
+++ .config/yadm/bootstrap.d/070-misc-utilities.sh##distro.Ubuntu,e.sh
@@ -4,11 +4,11 @@
 
 # [[file:../../../README.org::*Misc utilities][Misc utilities:1]]
 set -eux
-(command -v less &> /dev/null) || sudo apt-get install --yes less
-(command -v htop &> /dev/null) || sudo apt-get install --yes htop
-(command -v notify-send &> /dev/null) || sudo apt-get install --yes libnotify-bin
+(command -v less &> /dev/null) || sudo apt-get install --no-install-recommends -y less
+(command -v htop &> /dev/null) || sudo apt-get install --no-install-recommends -y htop
+(command -v notify-send &> /dev/null) || sudo apt-get install --no-install-recommends -y libnotify-bin
 # this is installed with apt-get because brew installs a shitload of dependencies
-(command -v svn &>/dev/null) || sudo apt-get install --yes subversion
+(command -v svn &>/dev/null) || sudo apt-get install --no-install-recommends -y subversion
 curl https://raw.githubusercontent.com/gitbits/git-info/master/git-info --output ~/.local/bin/git-info && chmod +x ~/.local/bin/git-info
 
 
