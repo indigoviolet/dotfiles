5796ca94dd663eaf9f1a0bbd4df7c7ea97202a49
Venky Iyer <indigoviolet@gmail.com>
2022-02-02 20:35:40 -0800

    gcm for git creds

--- .config/yadm/bootstrap.d/070-misc-utilities.sh##distro.Ubuntu,e.sh
+++ .config/yadm/bootstrap.d/070-misc-utilities.sh##distro.Ubuntu,e.sh
@@ -16,4 +16,9 @@ mkdir -p ~/.local/bin && curl -fsSL https://raw.githubusercontent.com/gitbits/gi
 ## poetry completion in prezto (https://python-poetry.org/docs/master/#enable-tab-completion-for-bash-fish-or-zsh)
 ## poetry installed with asdf
 poetry completions zsh > ~/.zprezto/modules/completion/external/src/_poetry
+
+## GCM core git credential helper (see https://blog.djnavarro.net/posts/2021-08-08_git-credential-helpers/)
+gcm_latest_release=$(curl -s https://api.github.com/repos/GitCredentialManager/git-credential-manager/releases/latest | jq -cr '.assets[] | select(.content_type | contains("deb")) | .browser_download_url')
+gcm_deb=$(curl -sw '%{filename_effective}' -LO $gcm_latest_release --output-dir /tmp)
+sudo dpkg -i $gcm_deb && rm $gcm_deb -f
 # Misc utilities:1 ends here
