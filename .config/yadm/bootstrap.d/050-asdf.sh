d72382e81796df72503c642774156f0fa8f72d46
Venky Iyer <indigoviolet@gmail.com>
2022-02-07 17:18:18 -0800

    use poetry 1.1.9 with install-poetry

--- .config/yadm/bootstrap.d/050-asdf.sh
+++ .config/yadm/bootstrap.d/050-asdf.sh
@@ -3,13 +3,13 @@
 
 # asdf installed with Brew
 
-
 # [[file:../../../README.org::*Asdf][Asdf:1]]
 set -ux
 
 ## https://github.com/asdf-vm/asdf/issues/276#issuecomment-907063520
-cut -d' ' -f1 .tool-versions | xargs -i asdf plugin add  {}
+cut -d' ' -f1 .tool-versions | xargs -i asdf plugin add {}
 
 ## installs from .tool-versions (which is an alt file)
-asdf install
+## the install-poetry installer is default with 1.2, but that is still alpha and has bugs <2022-02-07 Mon>
+ASDF_POETRY_INSTALL_URL=https://install.python-poetry.org asdf install
 # Asdf:1 ends here
