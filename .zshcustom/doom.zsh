83f6051c22911109ccdacedc72ae1ddf1cbf5ce1
Venky Iyer <indigoviolet@gmail.com>
2022-03-05 16:32:08 -0800

    fix doom install and env vars

--- .zshcustom/doom.zsh
+++ .zshcustom/doom.zsh
@@ -1,6 +1,4 @@
-EMACSDIR=$HOME/.emacs.d
+export EMACSDIR=$HOME/.emacs.d
+export DOOMDIR=$HOME/.config/doom
 path+=($EMACSDIR/bin)
 
-function doom-tangle-config() {
-    emacs --batch -Q --eval "(require 'org)" --eval '(org-babel-tangle-file "'$HOME'/.doom.d/config.org")'
-}
