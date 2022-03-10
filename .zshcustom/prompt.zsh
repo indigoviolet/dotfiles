fd26d968eba81f29155b4a3411a8cde7d78e22ff
Venky Iyer <indigoviolet@gmail.com>
2022-03-10 12:23:42 -0800

    fixes

--- .zshcustom/prompt.zsh
+++ .zshcustom/prompt.zsh
@@ -7,3 +7,7 @@ function set_window_title(){
     print -Pn "\e]2;%m:%2~\a"
 }
 precmd_functions+=(set_window_title)
+
+# colors xtrace prompts
+# https://unix.stackexchange.com/a/595628
+export PS4='%F{blue}%B+%N:%i>%b%f '
