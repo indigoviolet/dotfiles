31a136c1a7f8853e717853c48fd7fceb127260c4
Venky Iyer <indigoviolet@gmail.com>
2022-03-22 16:52:55 -0700

    fonts

--- .config/yadm/bootstrap.d/130-fonts.sh##c.personal,e.sh
+++ .config/yadm/bootstrap.d/130-fonts.sh##c.personal,e.sh
@@ -5,10 +5,22 @@
 
 
 # [[file:../../../README.org::*Fonts][Fonts:1]]
-set -eux
+set -euxo pipefail
 
 # Jetbrains Mono patched (https://github.com/ryanoasis/nerd-fonts#option-5-clone-the-repo)
-mkdir -p $HOME/dev && cd $HOME/dev && git clone --filter=blob:none --sparse git@github.com:ryanoasis/nerd-fonts
-cd $HOME/dev/nerd-fonts && git sparse-checkout add patched-fonts/JetBrainsMono
-./install.sh JetBrainsMono
+if [[ ! -d  $HOME/dev/nerd-fonts ]]; then
+    mkdir -p $HOME/dev
+    cd $HOME/dev
+    git clone --filter=blob:none --sparse git@github.com:ryanoasis/nerd-fonts
+else
+    cd $HOME/dev/nerd-fonts
+    git fetch
+fi
+
+for font in JetBrainsMono/Ligatures UbuntuMono IBMPlexMono VictorMono; do
+    git sparse-checkout add patched-fonts/$font
+    ./install.sh "${font%%/*}"
+done
+
+echo 'y' | emacs -l ~/.emacs.d/init.el --batch -f all-the-icons-install-fonts
 # Fonts:1 ends here
