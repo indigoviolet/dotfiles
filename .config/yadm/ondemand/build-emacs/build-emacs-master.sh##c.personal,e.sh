12ff60e37cd0dee75f9d4259cfd048fa86f6d7d6
Venky Iyer <indigoviolet@gmail.com>
2022-07-19 16:26:50 -0700

    pin emacs build to something that is working

--- .config/yadm/ondemand/build-emacs/build-emacs-master.sh##c.personal,e.sh
+++ .config/yadm/ondemand/build-emacs/build-emacs-master.sh##c.personal,e.sh
@@ -23,19 +23,19 @@ git fetch
 # echo 'Continue with build (RETURN to continue, ^C to quit)?'
 # read nought
 
-git pull --ff
-git clean -dfx
+git checkout master && git pull --ff && git clean -dfx && make clean
 
 ## on nativecomp: Pinned commit for https://discord.com/channels/406534637242810369/406536226166800384/797785171767197716
 # PINNED='213b5d73159cafbdd52b9c0fb0479544cca98a77'
-PINNED='HEAD'
+# PINNED='HEAD'
+PINNED='c5af19'
 
 echo "Using pinned commit $PINNED on $BUILD (RETURN to continue, ^C to quit)"
 read nought
 
 git checkout $PINNED
 
-sudo apt-get install --no-install-recommends -y libjansson4 libjansson-dev libgccjit0 libgccjit-11-dev gcc-11
+sudo apt-get install --no-install-recommends -y libjansson4 libjansson-dev libgccjit0 libgccjit-11-dev gcc-11 libwebp-dev
 
 # .zshcustom/build.zsh
 # 7:export PKG_CONFIG_PATH=/usr/lib/x86_64-linux-gnu/pkgconfig:/usr/share/pkgconfig
@@ -54,6 +54,9 @@ read nought
 echo Hit RETURN to install...
 read nought
 sudo make -j8 install
+
+installed_commit=$(git rev-parse --short HEAD)
+echo "Installed: $installed_commit" | ts >> build-emacs.log
 
 systemctl --user disable emacs.service
 sudo rm /usr/local/share/applications/{emacsclient,emacs-mail,emacsclient-mail}.desktop
