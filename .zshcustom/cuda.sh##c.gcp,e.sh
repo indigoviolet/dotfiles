9ea259f0a59f69e27214e4ac42a7d3014f63b63c
Venky Iyer <indigoviolet@gmail.com>
2022-08-24 12:07:01 -0700

    gcp cuda source

--- .zshcustom/cuda.sh##c.gcp,e.sh
+++ .zshcustom/cuda.sh##c.gcp,e.sh
@@ -1,2 +1,6 @@
 ## https://github.com/GoogleCloudPlatform/compute-gpu-installation/blob/32845620c0cc8168cd4a689dc7c3aab28afc4b35/linux/install_gpu_driver.py#L346
-source /etc/profile.d/cuda.sh
+if [[ -e "/etc/profile.d/cuda.sh" ]]; then
+    source /etc/profile.d/cuda.sh
+elif [[ -e "/etc/profile.d/nvidia-env.sh" ]]; then
+    source /etc/profile.d/nvidia-env.sh
+fi
