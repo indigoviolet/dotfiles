f2d1b712ac505e8498fc8af89cadd6a8dca9800c
Venky Iyer <indigoviolet@gmail.com>
2022-09-13 23:40:28 -0700

    get mamba install to use .local/ dir

--- .config/yadm/bootstrap.d/051-mamba-conda.sh
+++ .config/yadm/bootstrap.d/051-mamba-conda.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 # Mamba (conda)
 
+# https://github.com/conda-forge/miniforge#non-interactive-install
+
 
 # [[file:../../../README.org::*Mamba (conda)][Mamba (conda):1]]
 set -ux
+cd /tmp
 wget -O Mambaforge.sh "https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-$(uname)-$(uname -m).sh"
-bash Mambaforge.sh -b
+bash Mambaforge.sh -b -p $HOME/.local/mambaforge
 # Mamba (conda):1 ends here
