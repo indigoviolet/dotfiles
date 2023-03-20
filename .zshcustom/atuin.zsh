10d06c39163b387cf8657bec175d1238e792e257
Venky Iyer <indigoviolet@gmail.com>
2023-03-19 17:00:22 -0700

    atuinn works on two machines

--- .zshcustom/atuin.zsh
+++ .zshcustom/atuin.zsh
@@ -1,10 +1,11 @@
-export ATUIN_NOBIND="true"
+# fzf searching shows dupes, and skim is slower, and fzy doesn't have options
+# export ATUIN_NOBIND="true"
 eval "$(atuin init zsh)"
 
-zle -N _atuinr_widget _atuinr
+# zle -N _atuinr_widget _atuinr
 
-_atuinr() {
-    # https://github.com/ellie/atuin/issues/68
-    output=$(atuin history list --cmd-only | fzf --tac) && BUFFER=$output && CURSOR=${#BUFFER}
-}
-bindkey '^r' _atuinr_widget
+# _atuinr() {
+#     # https://github.com/ellie/atuin/issues/68
+#     output=$(atuin history list --cmd-only | fzf --tac) && BUFFER=$output && CURSOR=${#BUFFER}
+# }
+# bindkey '^r' _atuinr_widget
