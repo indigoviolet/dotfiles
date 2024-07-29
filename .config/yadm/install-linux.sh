88bbbaf349c0cd29c17961db12fe35acfecabdb7
Venky Iyer <indigoviolet@gmail.com>
2024-07-29 14:06:02 -0700

    new install scripts

---
+++ .config/yadm/install-linux.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# linux startup
+
+
+# [[file:../../dev/dotfiles/install.org::*linux startup][linux startup:1]]
+set -x
+set -euo pipefail
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install yadm just transcrypt
+# linux startup:1 ends here
