0157621d189191262ed1745f9f8a0a35032ccdc0
Venky Iyer <indigoviolet@gmail.com>
2024-08-09 10:14:52 -0700

    install scripts + bootstrap fixes

--- .config/yadm/install-darwin.sh
+++ .config/yadm/install-darwin.sh
@@ -5,6 +5,6 @@
 # [[file:../../dev/dotfiles/install.org::*mac startup][mac startup:1]]
 set -x
 set -euo pipefail
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+command -v brew || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install yadm just transcrypt
 # mac startup:1 ends here
