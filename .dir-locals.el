00a2e73b6f18ed7f42057a96276f1afcfb2043f5
Venky Iyer <indigoviolet@gmail.com>
2025-01-12 10:54:13 -0800

    no magit-todos

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,8 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((magit-status-mode . ((eval . (magit-disable-section-inserter 'magit-insert-ignored-files))))
+((magit-status-mode . (
+                        (eval . (magit-disable-section-inserter 'magit-insert-ignored-files))
+                        (magit-todos-exclude-globs . ("*"))
+                        )
+   ))
