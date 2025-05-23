48311c5366b2b9edcb4686fbf7f0f2f8623b36a9
Venky Iyer <indigoviolet@gmail.com>
2025-05-22 22:47:12 -0700

    fzf-tab fixes, lessopen

--- .zshcustom/less.zsh
+++ .zshcustom/less.zsh
@@ -1,2 +1,4 @@
 export LESS="-g -i -M -S --use-color -z-4 -R"
-export LESSOPEN="|$HOMEBREW_PREFIX/bin/lesspipe.sh %s"
+
+## Makes less slower
+# export LESSOPEN="|$HOMEBREW_PREFIX/bin/lesspipe.sh %s"
