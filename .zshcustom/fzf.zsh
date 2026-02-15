da3e19a62a9da94f0355cd92ef7f83860e10391c
Venky Iyer <indigoviolet@gmail.com>
2026-02-15 07:52:14 +0000

    Enable TPM with tmux-fzf plugin; modernize fzf.zsh setup

--- .zshcustom/fzf.zsh
+++ .zshcustom/fzf.zsh
@@ -1,27 +1,4 @@
-FZF_DIR="$HOMEBREW_PREFIX/opt/fzf"
-
-function setup_fzf() {
-
-    # Setup fzf
-    # ---------
-    if [[ ! "$PATH" == *$FZF_DIR/bin* ]]; then
-      path+=($FZF_DIR/bin)
-    fi
-
-    # Man path
-    # --------
-    if [[ ! "$MANPATH" == *$FZF_DIR/man* && -d "$FZF_DIR/man" ]]; then
-      export MANPATH="$MANPATH:$FZF_DIR/man"
-    fi
-
-    # Auto-completion
-    # ---------------
-    [[ $- == *i* ]] && source "$FZF_DIR/shell/completion.zsh" 2> /dev/null
-
-    # Key bindings
-    # ------------
-    source "$FZF_DIR/shell/key-bindings.zsh"
-    # bindkey '^G' fzf-file-widget
-}
-
-command -v fzf &> /dev/null && setup_fzf
+# Setup fzf (modern fzf generates shell config directly)
+if command -v fzf &> /dev/null; then
+    eval "$(fzf --zsh)"
+fi
